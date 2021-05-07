5e1be9342e027054620f88cb48f73abf012c2bd4
ovjunior7 <83840936+ovjunior7@users.noreply.github.com>
2021-05-07 16:06:38 -0400

    Adicionando arquivo java inicial
    Exemplo para aula de Engenharia de SOftware II.

---
+++ Fatorial.java
@@ -0,0 +1,22 @@
+
+package fatorial;
+
+public class Fatorial {
+
+   
+    public static void main(String[] args) {
+        String n="cunhad";
+        switch(n){
+            case "regiane","junior","osvaldo": System.out.println("Ola tudo bem");
+                        System.out.println("perfeito");
+                        break;
+            case "maria","roger","cunhada": System.out.println("como vai");
+                        System.out.println("legal");
+                        break;
+            default: System.out.println("Opçao invalida");
+                     System.out.println("errado");
+        }
+       
+    }
+    
+}
